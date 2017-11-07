13addae22894bb4362b605454e60100b34c3a489
vespertron <vesper.annstas@gmail.com>
2017-11-07 15:41:33 +0100

    Add Completed_at ability

---
+++ db/migrate/20171030083025_add_completed_at_items.rb
@@ -0,0 +1,4 @@
+class AddCompletedAtItems < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024103542) do
+ActiveRecord::Schema.define(version: 20171030083818) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171024103542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
